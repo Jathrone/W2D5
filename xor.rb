ce68d157df6bdb807196456abb566394d73827fb
Yucheng Li <jason.li.feed@gmail.com>
2019-03-22 10:46:16 -0700

    git init

---
+++ xor.rb
@@ -0,0 +1,37 @@
+require "set"
+def base_ten_xor(array1, array2)
+    #limit both arrays of numbers to first and tenth place
+    first_set = Set.new
+    tenth_set = Set.new
+    array1.each do |num1|
+        first, tenth = num1 % 10, num1 / 10
+        if first_set.include?(first)
+            first_set.delete(first)
+        else
+            first_set << first
+        end
+        if tenth_set.include?(tenth)
+            tenth_set.delete(tenth)
+        else
+            tenth_set << tenth
+        end
+    end
+    array2.each do |num2|
+        first, tenth = num2 % 10, num2 / 10
+        if first_set.include?(first)
+            first_set.delete(first)
+        else
+            first_set << first
+        end
+        if tenth_set.include?(tenth)
+            tenth_set.delete(tenth)
+        else
+            tenth_set << tenth
+        end
+    end
+    return [tenth_set, first_set]
+end
+
+arr1 = [11, 34, 89, 74, 55]
+arr2 = [11, 89, 74, 55]
+
